eb26208786e4527878b14f225b357115b295ef1a
yinzhiyu <yinzhiyu1@sensetime.com>
2024-10-25 19:25:52 +0800

    err unwrap support

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -1,49 +1,82 @@
 package errs
 
 import (
-	"container/list"
+	"errors"
 	"fmt"
 	"sync"
 )
 
 func New() *Errs {
-	return &Errs{
-		list: list.New(),
+	return &Errs{}
+}
+
+type ErrorChain struct {
+	cause *ErrorChain
+	Err   error
+	From  string
+}
+
+func (e *ErrorChain) Is(target error) bool {
+	return errors.Is(e.Err, target)
+}
+
+func (e *ErrorChain) As(target any) bool {
+	return errors.As(e.Err, target)
+}
+
+func (e *ErrorChain) Unwrap() error {
+	if e.cause == nil {
+		// 必需有这个，不能直接返回e.cause
+		// 因为nil和=nil的*ErrorChain是不同的，后者虽然是空指针但是实现了Unwrap方法，导致errors.Is和As无法正常结束
+		return nil
 	}
+	return e.cause
+}
+
+func (e *ErrorChain) Error() string {
+	return e.Err.Error()
 }
 
 type Errs struct {
 	lock sync.RWMutex
-	list *list.List
-}
-
-type ErrorInfo struct {
-	Err  error
-	From string
+	err  *ErrorChain
 }
 
 func (e *Errs) Add(err error, from string) {
+	if err == nil {
+		return
+	}
 	e.lock.Lock()
 	defer e.lock.Unlock()
-	e.list.PushBack(&ErrorInfo{err, from})
+	e.err = &ErrorChain{Err: err, From: from, cause: e.err}
+}
+
+func (e *Errs) Unwrap() error {
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	if e.err == nil {
+		return nil
+	}
+	return e.err
 }
 
 func (e *Errs) Has() bool {
 	e.lock.Lock()
 	defer e.lock.Unlock()
-	return e.list.Len() > 0
+	return e.err != nil
 }
 
 func (e *Errs) Error() string {
 	e.lock.Lock()
 	defer e.lock.Unlock()
 
-	info := fmt.Sprintf("%d errors occurred", e.list.Len())
-	cur := e.list.Front()
-	for i := 1; cur != nil; i++ {
-		v := cur.Value.(*ErrorInfo)
-		info += fmt.Sprintf("\n\nNO.%d: %s\n\thandler added from %s", i, v.Err, v.From)
-		cur = cur.Next()
+	var info string
+	cnt := 0
+	for cur := e.err; cur != nil; cur = cur.cause {
+		cnt++
+		info += fmt.Sprintf("\n\nNO.%d: %s\n\thandler added from %s", cnt, cur.Err, cur.From)
 	}
+	info = fmt.Sprintf("%d errors occurred", cnt) + info
+
 	return info
 }
